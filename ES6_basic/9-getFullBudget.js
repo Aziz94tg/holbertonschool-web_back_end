2a5da9cbd6b1cba90a5eaea942332407ee8df5ab
Aziz94tg <aziz94tg@gmail.com>
2025-08-20 15:12:46 +0300

    Add

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -1,9 +1,17 @@
-const fullBudget = {
-  ...budget,
-  getIncomeInDollars(income) {
-    return `$${income}`;
-  },
-  getIncomeInEuros(income) {
-    return `${income} euros`;
-  },
-};
+import getBudgetObject from './7-getBudgetObject';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars(income) {
+      return `$${income}`;
+    },
+    getIncomeInEuros(income) {
+      return `${income} euros`;
+    },
+  };
+
+  return fullBudget;
+}
